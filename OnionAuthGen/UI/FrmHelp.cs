836485910c912e8e396ee05a4b57c76057a351e2
Kevin Gut <kevin.gut@bitm.sg>
2021-10-27 14:41:19 +0200

    Implement simple chapter generator

--- OnionAuthGen/UI/FrmHelp.cs
+++ OnionAuthGen/UI/FrmHelp.cs
@@ -5,25 +5,64 @@ namespace OnionAuthGen
 {
     public partial class FrmHelp : Form
     {
+        private class HelpFile
+        {
+            public string ResourceName { get; private set; }
+            public string Title { get; private set; }
+
+            public HelpFile(string ResName)
+            {
+                ResourceName = ResName;
+                Title = GetText().Split('\n').First().Trim();
+            }
+
+            public string GetText()
+            {
+                return Tools.GetResourceText($"Help.{ResourceName}.txt");
+            }
+            public override string ToString()
+            {
+                if (string.IsNullOrEmpty(Title))
+                {
+                    if (string.IsNullOrEmpty(ResourceName))
+                    {
+                        return "Untitled help file";
+                    }
+                    return ResourceName;
+                }
+                return Title;
+            }
+        }
+
         public FrmHelp()
         {
             InitializeComponent();
             foreach (var s in Tools.GetResourceNames().Where(m => m.StartsWith("Help.") && m.EndsWith(".txt")))
             {
-                LbHelp.Items.Add(s.Substring(5, s.Length - 9));
+                LbHelp.Items.Add(new HelpFile(s.Substring(5, s.Length - 9)));
             }
             Tools.ScaleForm(this);
         }
 
         public void SetHelpArticle(string Name)
         {
-            //TbHelp.Text = Tools.GetResourceText($"Help.{Name}.txt");
-            LbHelp.SelectedItem = Name;
+            for(var i = 0; i < LbHelp.Items.Count; i++)
+            {
+                var H = (HelpFile)LbHelp.Items[i];
+                if (H.ResourceName == Name)
+                {
+                    LbHelp.SelectedIndex = i;
+                }
+            }
         }
 
         private void LbHelp_SelectedIndexChanged(object sender, System.EventArgs e)
         {
-            TbHelp.Text = Tools.GetResourceText($"Help.{LbHelp.SelectedItem}.txt");
+            if (LbHelp.SelectedIndex >= 0)
+            {
+                var H = (HelpFile)LbHelp.SelectedItem;
+                TbHelp.Text = H.GetText();
+            }
         }
     }
 }
